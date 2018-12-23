003f74f5edb1e1ee6c74a26c7169cf7006a4bcd2
Cédric Hulin <hulin.cedric@gmail.com>
2018-12-23 17:48:48 +0100

    test: Include MemberData to cover all data for the use case

--- FizzBuzz.Tests/FizzBuzzAnalyzerTests.cs
+++ FizzBuzz.Tests/FizzBuzzAnalyzerTests.cs
@@ -1,10 +1,26 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace FizzBuzz.Tests
 {
     public class FizzBuzzAnalyzerTests
     {
+        public static IEnumerable<object[]> OnlyMultiplesOf3 => from number in Enumerable.Range(1, 100)
+                                                                where number % 3 == 0 && number % 5 != 0
+                                                                select new object[] { number };
+        public static IEnumerable<object[]> OnlyMultiplesOf5 => from number in Enumerable.Range(1, 100)
+                                                                where number % 3 != 0 && number % 5 == 0
+                                                                select new object[] { number };
+
+        public static IEnumerable<object[]> MultiplesOf3And5 => from number in Enumerable.Range(1, 100)
+                                                                where number % 3 == 0 && number % 5 == 0
+                                                                select new object[] { number };
+        public static IEnumerable<object[]> NotMultiplesOf3And5 => from number in Enumerable.Range(1, 100)
+                                                                   where number % 3 != 0 && number % 5 != 0
+                                                                   select new object[] { number };
+
         [Fact]
         public void ShouldNotBeNull()
         {
@@ -14,59 +30,46 @@ namespace FizzBuzz.Tests
         }
 
         [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(4)]
-        [InlineData(7)]
-        public void ShouldReturnTheNumberWhenAnalyzeNotMultiplesOf3And5(int value)
+        [MemberData(nameof(NotMultiplesOf3And5))]
+        public void ShouldReturnTheNumberWhenAnalyzeNotMultiplesOf3And5(int number)
         {
             var analyzer = new Analyzer();
-            var expected = value.ToString();
+            var expected = number.ToString();
 
-            var actual = analyzer.Analyze(value);
+            var actual = analyzer.Analyze(number);
 
             Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData(3)]
-        [InlineData(6)]
-        [InlineData(9)]
-        [InlineData(12)]
-        public void ShouldReturnFizzWhenAnalyzeMultiplesOf3(int value)
+        [MemberData(nameof(OnlyMultiplesOf3))]
+        public void ShouldReturnFizzWhenAnalyzeMultiplesOf3(int number)
         {
             var analyzer = new Analyzer();
 
-            var actual = analyzer.Analyze(value);
+            var actual = analyzer.Analyze(number);
 
             Assert.Equal("Fizz", actual);
         }
 
         [Theory]
-        [InlineData(5)]
-        [InlineData(10)]
-        [InlineData(20)]
-        [InlineData(25)]
-        [InlineData(100)]
-        public void ShouldReturnFizzWhenAnalyzeMultiplesOf5(int value)
+        [MemberData(nameof(OnlyMultiplesOf5))]
+        public void ShouldReturnFizzWhenAnalyzeMultiplesOf5(int number)
         {
             var analyzer = new Analyzer();
 
-            var actual = analyzer.Analyze(value);
+            var actual = analyzer.Analyze(number);
 
             Assert.Equal("Buzz", actual);
         }
 
         [Theory]
-        [InlineData(15)]
-        [InlineData(30)]
-        [InlineData(45)]
-        [InlineData(60)]
-        public void ShouldReturnFizzWhenAnalyzeMultiplesOf3And5(int value)
+        [MemberData(nameof(MultiplesOf3And5))]
+        public void ShouldReturnFizzWhenAnalyzeMultiplesOf3And5(int number)
         {
             var analyzer = new Analyzer();
 
-            var actual = analyzer.Analyze(value);
+            var actual = analyzer.Analyze(number);
 
             Assert.Equal("FizzBuzz", actual);
         }
