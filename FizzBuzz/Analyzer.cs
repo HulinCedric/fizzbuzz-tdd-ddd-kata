4c23eac5add9e42ec45c4a03f478b090df5a9981
Cédric Hulin <hulin.cedric@gmail.com>
2018-12-23 18:21:19 +0100

    refactor: Transform rule immutable object into anemic value object

--- FizzBuzz/Analyzer.cs
+++ FizzBuzz/Analyzer.cs
@@ -4,11 +4,6 @@ namespace FizzBuzz
 {
     public class Analyzer
     {
-        private IEnumerable<Rule> rules => new List<Rule> {
-            new Rule(3, "Fizz"),
-            new Rule(5, "Buzz")
-        };
-
         public Analyzer()
         {
         }
@@ -16,14 +11,14 @@ namespace FizzBuzz
         public string Analyze(int number)
         {
             var result = default(string);
-            foreach (var rule in rules)
+            foreach (var rule in Rule.GetValues())
             {
                 if (number % rule.Value == 0)
                 {
                     result += rule.Output;
                 }
             }
-            
+
             return result ?? number.ToString();
         }
     }
--- FizzBuzz/Rule.cs
+++ FizzBuzz/Rule.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+
 namespace FizzBuzz
 {
     public class Rule
     {
+        private static Rule FizzRule { get; } = new Rule(3, "Fizz");
+        private static Rule BuzzRule { get; } = new Rule(5, "Buzz");
+
+        public static IEnumerable<Rule> GetValues()
+        {
+            yield return FizzRule;
+            yield return BuzzRule;
+        }
+
         public int Value { get; }
         public string Output { get; }
         public Rule(int value, string output)
